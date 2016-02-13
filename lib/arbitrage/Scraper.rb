72123581ba302b5e7070accda94f1a6e19abde86
corya0687 <corya0687@gmail.com>
2016-02-13 11:57:18 -0500

    Working added craigslist_query, buyotpons menue

--- lib/arbitrage/Scraper.rb
+++ lib/arbitrage/Scraper.rb
@@ -1,5 +1,5 @@
 class Arbitrage::Scraper
-  attr_accessor :url, :product
+  attr_accessor :url, :product, :buy_options
 
   def create_index_url
     csv_data = File.read('rsc/clzips.csv')
@@ -13,16 +13,43 @@ class Arbitrage::Scraper
     end
   end
 
-  def craigslist_search_url
-
-  end
-
-
   def create_product_list
+    craigslist_search_url
     html = open(self.url)
     doc = Nokogiri::HTML(html)
-    binding.pry
+    buy_options_menu(doc)
+
   end
+
+  def buy_options_menu(doc)
+    self.buy_options = {}
+    doc.css('.row').each_with_index do |row, i|
+      if i < 11
+        self.buy_options["#{i+1}".to_s] = {
+          name: (row.css('.hdrlnk').text),
+          price: (row.css('.l2 .price').text)
+        }
+        binding.pry
+      end
+    end
+  end
+
+  def craigslist_query
+    query = self.product.name.split(" ")
+    if query.size > 1
+      query= query.join("%20")
+    else
+      query= query.join
+    end
+  end
+
+  def craigslist_search_url
+    #binding.pry
+    self.url = "#{self.url}search/sss?query=#{craigslist_query}&sort=rel"
+  end
+
+
+
 
 
 end
